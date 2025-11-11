585f2856144afc9f7a35510fbf721085ab743c6f
Andrea Mascaretti <mascaretti@posteo.net>
2025-11-11 16:13:14 +0100

    chore: remove core boilerplate

--- src/bii/__init__.py
+++ src/bii/__init__.py
@@ -1,3 +1,10 @@
-from .core import normalize
-__all__ = ["normalize"]
+from .radial import (
+    make_distance,
+    equal_expected_count_shells_from_Rmax,
+    equal_expected_count_shells_via_lambda,
+    assign_to_shells_aligned,
+    sample_representatives_uniform_aligned,
+    make_partition_indices,
+    split_XZ_by_partition,
+)
 
